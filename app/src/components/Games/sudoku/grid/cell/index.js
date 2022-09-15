bd63acc00298f0505090e46252e05285fc67bd9b
ozdv <cam@ozdv.me>
2022-09-14 23:07:40 -0600

    Now works completely with text input

--- app/src/components/Games/sudoku/grid/cell/index.js
+++ app/src/components/Games/sudoku/grid/cell/index.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 require("./styles.scss");
 
 export default function Cell(props) {
-    const { value } = props;
-    console.log("cell", value);
-    return <div className="Cell">{value}</div>;
+    const { value, rowIndex, cellIndex } = props;
+    const [cellValue, setCellValue] = useState(value);
+
+    const cellValueHandler = (event) => {
+        if (event.nativeEvent.data === null) {
+            setCellValue("");
+        } else if (event.nativeEvent.data === 0) {
+            setCellValue(cellValue);
+        } else {
+            const newValue = event.nativeEvent.data.replace(
+                /[^1-9]/gi,
+                cellValue
+            );
+            setCellValue(newValue);
+        }
+    };
+    return (
+        <div className="Cell">
+            <input
+                id={`row${rowIndex}-cell${cellIndex}`}
+                className="cellInput"
+                type="text"
+                value={cellValue === 0 ? "" : cellValue}
+                onChange={cellValueHandler}
+            ></input>
+        </div>
+    );
 }
--- app/src/components/Games/sudoku/grid/row/index.js
+++ app/src/components/Games/sudoku/grid/row/index.js
@@ -4,11 +4,16 @@ import Cell from "../cell";
 require("./styles.scss");
 
 export default function Row(props) {
-    const { values } = props;
+    const { values, rowIndex } = props;
     return (
         <div className="Row">
-            {values.map((e) => (
-                <Cell value={e} />
+            {values.map((e, i) => (
+                <Cell
+                    key={`cell-${i}`}
+                    value={e}
+                    rowIndex={rowIndex}
+                    cellIndex={i}
+                />
             ))}
         </div>
     );
